ade96af52652a5a022d59bd6e3f37f0bee1eafa7
github username <github email address>
2017-08-14 16:11:56 -0400

    signup auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,4 +2,21 @@ class Api::V1::UsersController < ApplicationController
   def show
     render json: User.find(params[:id])
   end
+
+  def create
+    user = User.create(user_params)
+    if user.save
+      created_jwt = issue_token({id: user.id})
+      render json: {username: user.username,jwt: created_jwt}, status: 200
+    else
+      render json: {
+        error: 'User either exists or your password sucks'
+      }, status: 404
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:username, :email, :password)
+  end
 end
