0aa46aaf33046de1d7bf32a2bf1206e750d06890
github username <github email address>
2017-08-21 21:13:22 -0400

    commit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,6 +15,10 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def index 
+    render json: User.all
+  end
+
   private
   def user_params
     params.permit(:username, :email, :password)
